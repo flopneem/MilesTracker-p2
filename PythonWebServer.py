0a0efebd94180f488406ea0cda1bb9ec9c801ee3
flopneem <waszashibu@gmail.com>
2022-05-21 19:10:26 -0700

    This is the initial commit

---
+++ PythonWebServer.py
@@ -0,0 +1,23 @@
+# Python 3 server example
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return "Hello World"
+
+@app.route('/my')
+def my():
+    return "Hello again."
+
+@app.route('/myPath')
+def myPath():
+    return "Hello Path."
+
+if __name__ == '__main__':
+   app.run()
+
